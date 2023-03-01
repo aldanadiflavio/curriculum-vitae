7b5568a771239c5ca7786df144093eb516b96f67
Aldana <aldanadiflavio@gmail.com>
2023-03-01 00:10:35 -0300

    Creacion de la seccion educacion y se agrego boton para ir hasta arriba con javascript

--- js/script.js
+++ js/script.js
@@ -1,16 +1,36 @@
-// Obtener el enlace ancla
-const enlaceAncla = document.getElementById("exp-laboral");
+// Smooth Scroll
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
 
-// Agregar un evento de clic al enlace ancla
-enlaceAncla.addEventListener("click", function(event) {
-  // Prevenir el comportamiento predeterminado del enlace ancla
-  event.preventDefault();
-
-  // Obtener el destino del enlace ancla
-  const destino = this.getAttribute("href");
-
-  // Desplazarse suavemente al destino
-  document.querySelector(destino).scrollIntoView({
-    behavior: "smooth"
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
   });
 });
+
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 600);
+  
+  return false;
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+  $(".navbar-collapse").collapse('hide');});
